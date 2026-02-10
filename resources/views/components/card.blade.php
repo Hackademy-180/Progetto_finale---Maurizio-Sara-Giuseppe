13ef230a7f0cc1efd29cdba516a918aa5b9d1f12
Maurizio Catino <mauriziocatino96@gmail.com>
2026-02-10 16:45:58 +0100

    aggiustato altro frontend

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,10 +1,10 @@
-<div class="card card-custom swiper-custom" >
+<div class="card card-custom " >
   <img src="https://picsum.photos/500" class="" alt="...">
   <div class="card-body">
     <h5 class="card-title">{{$article->title}}</h5>
     <div class="d-flex flex-column justify-content-between">
       <h4 class="mb-4">{{$article->price}} €</h4>
-      <a href="{{route('byCategory', ["category" => $article->category])}}" class="btn-custom-1 btn btn-danger mx-1 mb-3"><em>{{$article->category->name}}</em> </a>
+      <a href="{{route('byCategory', ["category" => $article->category])}}" class="btn-custom-1 mx-1 mb-3 ">{{$article->category->name}} </a>
       <a href={{route("show_article", compact("article"))}} class="btn-custom btn btn-danger"> Dettaglio </a>
 
     </div>
