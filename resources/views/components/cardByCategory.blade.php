7a751a9504aa22c564151444f0b68384aff95dbd
spadafinasara <spadafina.sara@gmail.com>
2026-02-10 11:54:11 +0100

    problema card categoria

--- resources/views/components/cardByCategory.blade.php
+++ resources/views/components/cardByCategory.blade.php
@@ -1,8 +1,10 @@
 <div class="card" >
   <img src="https://picsum.photos/200" class="" alt="...">
-  <div class="card-body bg-danger">
+  <div class="card-body">
     <h5 class="card-title">{{$article->title}}</h5>
-    <h4 class="card-subtitle text-body-secondary">{{$article->price}} €</h4>
-    <a href={{route("show_article", compact("article"))}} class="btn btn-primary">Dettaglio</a>
+    <div class="d-flex flex-column justify-content-between card-custom-1">
+      <h4 class="card-subtitle text-body-secondary">{{$article->price}} €</h4>
+      <a href={{route("show_article", compact("article"))}} class="btn btn-primary">Dettaglio</a>
+    </div>
   </div>
 </div>
