3889a1ba71950be72fe15f3835d123755d60152e
spadafinasara <spadafina.sara@gmail.com>
2026-02-16 15:36:46 +0100

    user story 8

--- app/Livewire/ArticleForm.php
+++ app/Livewire/ArticleForm.php
@@ -10,6 +10,8 @@ use Livewire\Attributes\Validate;
 use Livewire\Component;
 use Livewire\WithFileUploads;
 
+use App\Jobs\RemoveFaces;
+
 class ArticleForm extends Component
 {
 
@@ -46,7 +48,12 @@ class ArticleForm extends Component
             foreach($this->images as $image) {
                 $newFileName = "articles/{$this->article->id}";
                 $newImage = $this->article->images()->create(['path' => $image->store($newFileName, 'public')]);
-                dispatch(new ResizeImage($newImage->path, 300, 300));
+                
+                RemoveFaces::withChain([
+                new ResizeImage($newImage->path, 300, 300),
+                // new GoogleVisionSafeSearch($newImage->id),
+                // new GoogleVisionLabelImage($newImage->id)
+            ])->dispatch($newImage->id);
             }
             File::deleteDirectory(storage_path('/app/livewire-tmp'));
         }    
