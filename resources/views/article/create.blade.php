7f33f49b45cf036e24a9a62867fa62a200024c90
spadafinasara <spadafina.sara@gmail.com>
2026-02-16 16:26:52 +0100

    crea articolo

--- resources/views/article/create.blade.php
+++ resources/views/article/create.blade.php
@@ -5,5 +5,16 @@
         </div>
     @endif
     <h1 class="text-center display-2 lead m-0 pt-4">{{ __('ui.Crea articolo') }}</h1>
-    <livewire:article-form/>
+
+      {{-- <div class="header-img col-12 d-flex "> --}}
+    <main class="container">
+        <section class="row d-flex">
+            <article class=" col-12 d-flex"> 
+                <div class="col-6">
+                    <livewire:article-form/>
+                </div>
+                <div class="crea-articolo-img col-6"></div>
+            </article>
+        </section>
+    </main>
 </x-layout>
